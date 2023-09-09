7acf0e7a5837689664fdd9e5d9299fe5383dc907
Sabyrzhan Kadir <sab.sabyr@gmail.com>
2023-09-09 23:34:23 +0600

    title description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,15 @@ import Introduction from '../components/introduction'
 import Characteristics from '../components/characteristics'
 import Modules from '../components/modules'
 import Footer from '../components/footer'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Shabyt Robotics</title>
+        <meta name="description" content="Shabyt Robotics official web-site" />
+      </Head>
       <TitleSlide />
       <Introduction />
       <Characteristics />
